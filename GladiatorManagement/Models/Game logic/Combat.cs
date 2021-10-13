6f2708f42965b3744ffb3d5e9d3eab01b98fe7d0
Shikyo <dennan_lol@hotmail.com>
2021-10-13 11:45:34 +0200

    Beginning of combat

---
+++ GladiatorManagement/Models/Game logic/Combat.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladiatorManagement.Models.Game_logic
+{
+    public class Combat
+    {
+        private PlayerGladiator player;
+        private Gladiator opponent;  
+
+        public Combat(PlayerGladiator player, Gladiator opponent)
+        {
+            this.player = player;
+            this.opponent = opponent;
+        }
+
+        public List<CombatInfo> StartCombat()
+        {
+            bool combatInprogress = true;
+            List<CombatInfo> combatDetails = new List<CombatInfo>();
+
+            while(combatInprogress)
+            {
+                CombatInfo combatDetails = ;
+
+                combatInprogress = CheckHealth(combatDetails);
+            }
+
+            return combatDetails;
+        }
+
+        private bool CheckHealth(CombatInfo combatDetails)
+        {
+            if (opponent.Health <= 0)
+            {
+                //combatDetails.Winner = Player;
+                return false;
+            }
+            else if (player.Health <= 0)
+            {
+                //combatDetails.Winner = Opponent;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ GladiatorManagement/Models/Game logic/CombatInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladiatorManagement.Models.Game_logic
+{
+    public class CombatInfo
+    {
+        public int PlayerRollResult { get; set; }
+        public int OpponentRollResult { get; set; }
+
+        public string PlayerRollDetails { get; set; }
+        public string OpponentRollDetails { get; set; }
+
+        
+
+    }
+}
