9f7a1ea7bebed4bc7a69a5446dae43ec4f4640ca
Marja Rolandsson <marja.rolandsson@gmail.com>
2021-10-14 14:44:38 +0200

    Changed CreateOpponent to be randomly generated based on a PlayerGladiator

--- GladiatorManagement/Models/Service/PlayerService.cs
+++ GladiatorManagement/Models/Service/PlayerService.cs
@@ -1,4 +1,5 @@
 ﻿using GladiatorManagement.Data;
+using Lexicon.CSharp.InfoGenerator;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,14 @@ namespace GladiatorManagement.Models.Service
     public class PlayerService
     {
         PlayerRepo _playerRepo;
-        PlayerGladiatorRepo _gladiatorRepo;
+        PlayerGladiatorRepo _playerGladiatorRepo;
+        GladiatorRepo _gladiatorRepo;
+        static InfoGenerator generator = new InfoGenerator();
 
-        public PlayerService(PlayerRepo playerRepo, PlayerGladiatorRepo gladiatorRepo)
+        public PlayerService(PlayerRepo playerRepo, PlayerGladiatorRepo playerGladiatorRepo, GladiatorRepo gladiatorRepo)
         {
             _playerRepo = playerRepo;
+            _playerGladiatorRepo = playerGladiatorRepo;
             _gladiatorRepo = gladiatorRepo;
         }
 
@@ -26,10 +30,34 @@ namespace GladiatorManagement.Models.Service
             int health = 1;
             int defence = 1;
 
-            PlayerGladiator gladiator = _gladiatorRepo.Create(name, strength, accuracy, health, defence);
+            PlayerGladiator gladiator = _playerGladiatorRepo.Create(name, strength, accuracy, health, defence);
 
             return gladiator;
 
+        }
+
+        public Gladiator CreateOpponent(PlayerGladiator playerGladiator)
+        {
+            Random rng = new Random();
+
+            string name = generator.NextString(3, 8);
+
+            int minStr = playerGladiator.Strength - 2;
+            int maxStr = playerGladiator.Strength + 3;
+            int minAcc = playerGladiator.Accuracy - 2;
+            int maxAcc = playerGladiator.Accuracy + 3;
+            int minHealth = playerGladiator.Health - 2;
+            int maxHealth = playerGladiator.Health + 3;
+            int minDef = playerGladiator.Defence - 2;
+            int maxDef = playerGladiator.Defence + 3;
+
+
+            int strength = rng.Next(minStr, maxStr);
+            int accuracy = rng.Next(minAcc, maxAcc);
+            int health = rng.Next(minHealth, maxHealth);
+            int defence = rng.Next(minDef, maxDef);
+
+            return _gladiatorRepo.Create(name, strength, accuracy, health, defence);
         }
 
         public PlayerGladiator UpdateGladiatorGear(PlayerGladiator gladiator, Gear gear)
@@ -37,10 +65,11 @@ namespace GladiatorManagement.Models.Service
             if (gear is Armor) gladiator.Armor = (Armor)gear;
             else if (gear is Weapon) gladiator.Weapon = (Weapon)gear;    
             
-            return _gladiatorRepo.Update(gladiator);
+            return _playerGladiatorRepo.Update(gladiator);
 
         }
         
+        //send in negative value for changeInGold if you want to decrease amount
         public Player EditAmountOfGold(Player player, int changeInGold)
         {
             player.Gold += changeInGold;
@@ -56,8 +85,13 @@ namespace GladiatorManagement.Models.Service
 
         public PlayerGladiator LevelUp(PlayerGladiator playerGladiator)
         {
-            playerGladiator.Level++;
-            return _gladiatorRepo.Update(playerGladiator);
+            if (canLevelUp(playerGladiator)) playerGladiator.Level++;
+            return _playerGladiatorRepo.Update(playerGladiator);
+        }
+
+        public bool canLevelUp(PlayerGladiator gladiator)
+        {
+            return true;
         }
 
 
