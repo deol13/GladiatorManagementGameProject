cf0275b82e5fa556afcb80420c4025863bb5392c
Shikyo <dennan_lol@hotmail.com>
2021-10-14 14:12:22 +0200

    Added a text to XpToLVl and GoldToGive so when you hover over them you get an explanation. And Game service gives out gold and lvl but not death.

--- GladiatorManagement/Models/Game logic/GameService.cs
+++ GladiatorManagement/Models/Game logic/GameService.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GladiatorManagement.Models.Service;
 
 namespace GladiatorManagement.Models.Game_logic
 {
     public class GameService
     {
-        public GameService()
-        {
+        PlayerService _playerService;
 
+        public GameService(PlayerService playerService)
+        {
+            _playerService = playerService;
         }
 
         public void LaunchCombat(int playerGladiatorId, int opponentGladiatorId)
@@ -53,12 +56,25 @@ namespace GladiatorManagement.Models.Game_logic
 
             List<CombatInfo> listOfCombatDetails = combat.StartCombat();
 
+            CombatInfo info = listOfCombatDetails.Last();
+
+            
             if (p != null)
             {
                 player.Strength -= p.Weapon.Strength;
                 player.Accuracy -= p.Weapon.Accuracy;
                 player.Defence -= p.Armor.Defence;
                 player.Health -= p.Armor.Health;
+
+                //Work in progress
+                if(info.Winner == "Player")
+                {
+                    int gold = HowMuchGoldWon(p.Level);
+                    _playerService.EditAmountOfGold(p.Player, gold);
+                    _playerService.LevelUp(p);
+
+                    //_playerService.RemoveGladiator(opponent);
+                }
             }
             if (o != null)
             {
@@ -66,12 +82,37 @@ namespace GladiatorManagement.Models.Game_logic
                 opponent.Accuracy -= o.Weapon.Accuracy;
                 opponent.Defence -= o.Armor.Defence;
                 opponent.Health -= o.Armor.Health;
+
+                //Work in progress
+                if (info.Winner == "Opponent")
+                {
+                    int gold = HowMuchGoldWon(o.Level);
+                    _playerService.EditAmountOfGold(o.Player, gold);
+                    _playerService.LevelUp(o);
+                }
+
+                //_playerService.RemoveGladiator(player);
             }
 
             //TODO
-            //Give gold
-            //Give Xp
-            //Remove players char if they died
+            //Remove gladiator when they die
+        }
+
+        public int HowMuchGoldWon(int lvl)
+        {
+            int gold = 0;
+
+            if ((lvl-1) > 0 && (lvl-1) < XPAndGoldFormula.GoldToGive.Length)
+                gold = XPAndGoldFormula.GoldToGive[lvl-1];
+
+            return gold;
+        }
+
+        public ShopInventory GenerateInventoryForAShop(ShopInventory inventory)
+        {
+
+
+            return inventory;
         }
         
     }
--- GladiatorManagement/Models/Game logic/XPAndGoldFormula.cs
+++ GladiatorManagement/Models/Game logic/XPAndGoldFormula.cs
@@ -10,7 +10,17 @@ namespace GladiatorManagement.Models.Game_logic
         private static Random rng;
         private static int MaxLvl { get; set; } = 20;
 
+        /// <summary>
+        /// Index 0 is the xp amount to lvl 2, index 1 to lvl 3 and so on.
+        /// So use lvl-1. For example if you are lvl 1, you do XpToLVl[lvl-1], lvl-1=0 and index 0 is
+        /// The amount to lvl 2.
+        /// </summary>
         public static int[] XpToLVl { get; set; }
+        /// <summary>
+        /// The amount of gold an enemy of a specific lvl gives, index 0 is for a lvl 1 enemy.
+        /// So use lvl-1. For example, you defeated a lvl 1 enemy, you do GoldToGive[lvl-1], lvl-1=0 and index 0
+        /// is for a lvl 1 enemy.
+        /// </summary>
         public static int[] GoldToGive { get; set; }
 
         public static void Setup()
